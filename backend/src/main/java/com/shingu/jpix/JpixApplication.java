70db82fbcf5279540c47f1094db660e12975c264
j1sooo <kjisoo@gmail.com>
2024-05-16 21:31:41 +0900

    new

---
+++ backend/src/main/java/com/shingu/jpix/JpixApplication.java
@@ -0,0 +1,13 @@
+package com.shingu.jpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shingu/jpix/JpixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shingu.jpix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
