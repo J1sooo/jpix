32f2b1841251b21555590221d24501d6febc97e9
junbeom1128 <jbkim4317@g.shingu.ac.kr>
2024-05-23 11:15:54 +0900

    Add files via upload

---
+++ src/Following/Following.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Following() {
+  return (
+    <div>
+      <h2>팔로잉 페이지</h2>
+      <p>팔로우한 사용자를 여기에 표시합니다.</p>
+    </div>
+  );
+}
+
+export default Following;
